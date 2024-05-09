d6a5239c808934a89f3ce89864553b0f1a558251
Skun01 <truongg9655@gmail.com>
2024-05-09 16:24:38 +0700

    make the keyboard can interact with calc

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,14 +9,30 @@ const acBtnElem = btnSectionElem.querySelector('#ac');
 const typeElem = inputScreenElem.querySelector('.typing');
 let userInput = '', history = '';
 
+const mapBtn = {
+    1 : "one",
+    2 : "two",
+    3 : "three",
+    4 : "four",
+    5 : "five",
+    6 : "six",
+    7 : "seven",
+    8 : "eight",
+    9 : "nine",
+    0 : "zero",
+    "." : "dot",
+    "+" : "plus",
+    "-" : "minus",
+    "*" : "multiply",
+    "/" : "divide",
+    Backspace : "backspace",
+    Enter : "equal",
+    "(" : "open-par",
+    ")" : "close-par",
+}
+
 btnSectionElem.addEventListener('click', e=>{
-    if([...e.target.classList].includes('row')) return;
-    switch(e.target){
-        case backspaceBtnElem:
-        case equalBtnElem:
-        case acBtnElem:
-            return;
-    }
+    if(![...e.target.classList].includes('display-btn')) return;
     if(userInput.length >= 39) alert('Full');
     else{
         userInput += e.target.textContent;
@@ -32,7 +48,6 @@ acBtnElem.addEventListener('click', e=>{
 });
 
 backspaceBtnElem.addEventListener('click', e=>{
-    e.preventDefault();
     if(userInput.length > 0){
         const tmp = [...userInput];
         tmp.pop();
@@ -42,5 +57,31 @@ backspaceBtnElem.addEventListener('click', e=>{
 });
 
 equalBtnElem.addEventListener('click', e=>{
-    
+    const infixEx = seperExpression(userInput);
+    console.log(infixEx);
 });
+
+document.body.addEventListener('keydown', e=>{
+    if(mapBtn[e.key] !== undefined){
+        console.log(mapBtn[e.key]);
+        const elem = document.querySelector(`#${mapBtn[e.key]}`);
+        elem.dispatchEvent(new Event('click', {bubbles: true}));
+    }
+});
+
+function seperExpression(str){
+    const ans = [];
+    const regex = /[\+\-\*\/]/g;
+    let curr = '';
+    for(let i = 0; i < str.length; i++){
+        if(regex.test(str[i])){
+            if(curr){
+                ans.push(curr);
+                curr = '';
+            }
+            ans.push(str[i]);
+        }else curr += str[i];
+    }
+    if(curr) ans.push(curr);
+    return ans;
+}
